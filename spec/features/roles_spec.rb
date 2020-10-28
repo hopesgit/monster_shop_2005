eabbc703b6052f9bb4e3c3ab077a0698d118dd3f
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-28 17:22:11 -0600

    add test for default user to not be able to access merchant/admin pages

--- spec/features/roles_spec.rb
+++ spec/features/roles_spec.rb
@@ -6,7 +6,7 @@ describe "At various user levels" do
       allow_any_instance_of(ApplicationController).to receive(:current_user)
 
       visit "/merchant"
-      save_and_open_page
+
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
 
       visit "/admin"
@@ -14,6 +14,34 @@ describe "At various user levels" do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
 
       visit "/profile"
+
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end
+  end
+
+  describe "As a User" do
+    it "gives you a 404 error when going to these pages" do
+      user_2 = User.create!(name: "George",
+                            street_address: "123 lane",
+                            city: "Denver",
+                            state: "CO",
+                            zip: 80111,
+                            email_address: "Todd@example.com",
+                            password: "superEasyPZ")
+
+      visit "/items"
+      click_link "Log In"
+      fill_in("Email Address", with: "#{user_2.email_address}")
+      fill_in("Password", with: "#{user_2.password}")
+      click_button("Submit")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user)
+
+      visit "/merchant"
+
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit "/admin"
 
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end
