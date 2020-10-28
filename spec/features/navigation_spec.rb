cf61769d1429b12dd547c15c38b30298c7a002ef
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-27 20:20:18 -0600

    Add testing for navbar links and welcome index files

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -33,5 +33,35 @@ RSpec.describe 'Site Navigation' do
       end
 
     end
+
+    it "I see a link to head back to the home page" do
+      visit '/merchants'
+
+      within 'nav' do
+        click_on "Home"
+      end
+
+      expect(current_path).to eq("/")
+
+      visit '/items'
+
+      within 'nav' do
+        click_on "Home"
+      end
+
+      expect(current_path).to eq("/")
+    end
+
+    it "I see a link to see what's in my shopping cart" do
+      
+    end
+
+    it "I see a link to log in on all pages" do
+
+    end
+
+    it "I see a link to register on all pages" do
+
+    end
   end
 end
