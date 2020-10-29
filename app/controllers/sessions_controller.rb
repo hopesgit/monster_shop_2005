724dc2b0b486d3a4bfdcf134aa0a8dfb45305e87
George Soderholm <georgesoderholm@gmail.com>
2020-10-28 20:10:15 -0600

    Add code to add a flash message for already being logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def new
+    if current_user
+      flash[:error] = 'You are already logged in!'
+      redirect_to '/profile'
+    end
   end
 
   def create
