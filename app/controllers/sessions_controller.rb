9c9acbfff573044201bfa94bb844980266bafbe2
George Soderholm <georgesoderholm@gmail.com>
2020-10-28 19:24:00 -0600

    Add an if statement to the create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email_address: params[:email_address])
-    if user.authenticate(params[:password])
+    if user.authenticate(params[:password]) && user.default?
       session[:user_id] = user.id
       redirect_to "/profile"
+    elsif user.authenticate(params[:password]) && user.merchant?
+      session[:user_id] = user.id
+      redirect_to '/merchant'
+    elsif user.authenticate(params[:password]) && user.admin?
+      session[:user_id] = user.id
+      redirect_to "/admin"
     else
       flash.now[:notice] = "Incorrect email/password combination."
       render :new
