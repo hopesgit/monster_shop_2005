c2adcf012d87958efe79ec552412bf7c8e37dce5
George Soderholm <georgesoderholm@gmail.com>
2020-10-29 21:35:15 -0600

    Add binding pry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController<ApplicationController
       @merchant = Merchant.find(params[:merchant_id])
       @items = @merchant.items
     else
+      binding.pry
       @items = Item.all
     end
   end
