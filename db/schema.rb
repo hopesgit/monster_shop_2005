2f4d87a429b151b3e1dd180f048809f62f581606
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-31 14:14:41 -0600

    Adds status column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_211927) do
+ActiveRecord::Schema.define(version: 2020_10_31_201347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_211927) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "reviews", force: :cascade do |t|
