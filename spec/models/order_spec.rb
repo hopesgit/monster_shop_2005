f1b3cf04f58dbcf3a8d504e1043c1cdea88273d6
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-04 14:46:14 -0700

    Add testing for Order#all_fulfilled? and #packaged_status methods

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -30,19 +30,42 @@ describe Order, type: :model do
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
 
       @order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user_1.id)
 
-      @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
-      @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3)
+      @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2, status: "fulfilled")
+      @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3, status: "fulfilled")
+
+      @order_2 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user_1.id)
+
+      @order_2.item_orders.create!(item: @tire, price: @tire.price, quantity: 2, status: "fulfilled")
+      @order_2.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3, status: "fulfilled")
+
+      @order_2.item_orders.create!(item: @dog_bone, price: @dog_bone.price, quantity: 3)
     end
 
     it '#grandtotal' do
       expect(@order_1.grandtotal).to eq(230)
     end
-    
+
     it "#total_order_items" do
       expect(@order_1.total_order_items).to eq(5)
+    end
+
+    it '#all_fulfilled?' do
+      expect(@order_1.all_fulfilled?).to eq(true)
+      expect(@order_2.all_fulfilled?).to eq(false)
+    end
+
+    it '#packaged_status' do
+      expect(@order_1.status).to eq("pending")
+      @order_1.packaged_status
+      expect(@order_1.status).to eq("packaged")
+
+      expect(@order_2.status).to eq("pending")
+      @order_2.packaged_status
+      expect(@order_2.status).to eq("pending")
     end
   end
 end
