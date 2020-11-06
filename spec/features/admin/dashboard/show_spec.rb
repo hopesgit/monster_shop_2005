8da350595f389fd684a3a6fb12e2976aea7127a1
George Soderholm <georgesoderholm@gmail.com>
2020-11-05 21:46:16 -0700

    Add merchant_id to users

--- spec/features/admin/dashboard/show_spec.rb
+++ spec/features/admin/dashboard/show_spec.rb
@@ -4,15 +4,16 @@ describe "As an Admin user" do
   describe "When I visit my dashboard" do
 
     before :each do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @user_1 = User.create!(name: "George",
                             street_address: "123 lane",
                             city: "Denver",
                             state: "CO",
                             zip: 80111,
                             email_address: "George@example.com",
-                            password: "superEasyPZ")
+                            password: "superEasyPZ",
+                            merchant_id: @meg.id)
 
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
@@ -38,7 +39,8 @@ describe "As an Admin user" do
                             zip: 80111,
                             email_address: "Todd@example.com",
                             password: "superEasyPZ",
-                            role: 2)
+                            role: 2,
+                            merchant_id: @meg.id)
 
       visit("/login")
       click_link "Log In"
