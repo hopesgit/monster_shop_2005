ffce70750d8e180ea0df34ef100aa0db4390a1ea
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-05 19:44:31 -0700

    Add "Active?" column to Merchants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_184919) do
+ActiveRecord::Schema.define(version: 2020_11_06_021814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_184919) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active?", default: true
   end
 
   create_table "orders", force: :cascade do |t|
