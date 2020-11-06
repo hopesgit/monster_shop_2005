dff9d88c115c0bb5202323913038b8ea57989ff9
George Soderholm <georgesoderholm@gmail.com>
2020-11-05 20:23:54 -0700

    Add merchant_id to the user

--- spec/features/sessions/logout_spec.rb
+++ spec/features/sessions/logout_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Logout' do
   describe 'When I visit the logout path' do
     before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
       @user_3 = User.create!(name: "Hope",
                             street_address: "222 Hope Ln",
                             city: "Denver",
@@ -10,7 +11,8 @@ RSpec.describe 'Logout' do
                             zip: 80112,
                             email_address: "hope@example.com",
                             password: "supersecret",
-                            role: 1)
+                            role: 1,
+                            merchant_id: @bike_shop.id)
       visit "/login"
 
       click_link "Log In"
